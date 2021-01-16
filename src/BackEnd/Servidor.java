0150bb4b05e66e033ace412a58cfc99bee3b5cc4
Pedro Magalhães <49410762+nonvegan@users.noreply.github.com>
2021-01-16 02:19:00 +0000

    Servidor RMI implementado na aplicação

--- src/BackEnd/Servidor.java
+++ src/BackEnd/Servidor.java
@@ -4,11 +4,12 @@ import java.net.*;
 import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
-public class Servidor extends Thread{
+public class Servidor extends Thread {
 
     Sistema sistema;
-    
+
     public Servidor(Sistema sistema) {
         this.sistema = sistema;
     }
@@ -18,9 +19,13 @@ public class Servidor extends Thread{
         ServerSocket servidor = null;
         try {
             servidor = new ServerSocket(sistema.getCurrentUser().getPorta());
+        } catch (BindException e) {
+            JOptionPane.showMessageDialog(sistema.getJanela(), "A porta " + sistema.getCurrentUser().getPorta() + " já se encontra em uso, por favor feche aplicação aberta.", "Porta em uso", JOptionPane.ERROR_MESSAGE);
+            System.exit(1);
         } catch (IOException ex) {
             Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
         }
+
         sistema.getJanela().setVisible(true);
 
         while (true) {
@@ -35,7 +40,5 @@ public class Servidor extends Thread{
 
         }
     }
-
-    
 
 }
